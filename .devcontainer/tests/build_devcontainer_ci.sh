7fe5a6dbe3530a5d128dfb3afd1c958435b0f08d
chris.anatalio <chris.anatalio@stellar.org>
2025-04-08 02:32:07 -0700

    Tweak vscode settings for perf
    Signed-off-by: chris.anatalio <chris.anatalio@stellar.org>

--- .devcontainer/tests/build_devcontainer_ci.sh
+++ .devcontainer/tests/build_devcontainer_ci.sh
@@ -6,27 +6,8 @@ devcontainer_dir=".devcontainer"
 # Define configuration file path
 config_file="devcontainer.json"
 
-# Use env vars or enter in your own values here.
-###
-
-# Example
-# - buildpack-deps:bookworm
-# - buildpack-deps:bookworm2
-# - /User/cache
-
 pre_build_image=$1
 oci_pre_build_image=$2
-
-build_details_dir="z-dc-build-info/"
-build_details_file="build-details.json"
-
-if [ ! -e "${build_details_dir}" ]; then
-  mkdir -p "${build_details_dir}"
-fi
-
-if [ ! -e "${build_details_dir}${build_details_file}" ]; then
-  touch "${build_details_dir}${build_details_file}"
-fi
 
 # Build the devcontainer
 output=$(devcontainer build \
@@ -34,30 +15,16 @@ output=$(devcontainer build \
   --config $devcontainer_dir/$config_file \
   --output type=image,name="${pre_build_image}")
 
-#--dotfiles-repository
-
 # Check the exit status and push pre-build
 if [ "$output" ]; then
   echo " ✅ Devcontainer built successfully"
 
-  # Extract imageName from JSON output using jq
   image_name=$(echo "$output" | jq -r '.imageName[0]')
   echo " 🔹 Image name: ${image_name}"
-  docker inspect "${image_name}" >> "${build_details_dir}${build_details_file}"
-
-  echo " ⚙️ Build info available at ${build_details_dir}${build_details_file}"
+  docker inspect "${image_name}"
 
 else
   echo " ❌ Error building devcontainer. Please check logs above."
   exit 1
 fi
-
-# Build the devcontainer again with OCI Output
-oci_output=$(devcontainer build \
-  --workspace-folder . \
-  --config $devcontainer_dir/$config_file \
-  --output type=image,name="${oci_pre_build_image}",mode=max,oci-mediatypes=true,compression=zstd)
-
-# Extract ociImageName from JSON output using jq
-oci_image_name=$(echo "$oci_output" | jq -r '.imageName[0]')
 
