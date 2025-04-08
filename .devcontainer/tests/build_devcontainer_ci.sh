6a826f8c577ac4780cb2c0822158d969a317d281
chris.anatalio <chris.anatalio@stellar.org>
2025-04-08 03:17:12 -0700

    Fix GH Action
    Signed-off-by: chris.anatalio <chris.anatalio@stellar.org>

--- .devcontainer/tests/build_devcontainer_ci.sh
+++ .devcontainer/tests/build_devcontainer_ci.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-
+-set e
 # Define devcontainer configuration directory
 devcontainer_dir=".devcontainer"
 
 # Define configuration file path
 config_file="devcontainer.json"
 
-pre_build_image=$1
-oci_pre_build_image=$2
-
 # Build the devcontainer
-output=$(devcontainer build \
+devcontainer build \
   --workspace-folder . \
-  --config $devcontainer_dir/$config_file \
-  --output type=image,name="${pre_build_image}")
+  --config $devcontainer_dir/$config_file
 
-# Check the exit status and push pre-build
-if [ "$output" ]; then
-  echo " ✅ Devcontainer built successfully"
 
-else
-  echo " ❌ Error building devcontainer."
-  exit 1
-fi
 
