1956f8866d84b04a9d237bc2cec4aab825553c52
Chris.Anatalio <chris.anatalio@stellar.org>
2025-05-23 04:27:02 -0700

    Prewarm cache and cleanup
    Signed-off-by: Chris.Anatalio <chris.anatalio@stellar.org>

--- .devcontainer/ps_autocomplete.sh
+++ .devcontainer/ps_autocomplete.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Add Stellar CLI to $PATH
-chmod 644 ~/.bashrc && chmod 644 ~/.zshrc
+chmod 775 ~/.bashrc && chmod 775 ~/.zshrc
 
 test -d /usr/local/bin/ && echo "/usr/local/bin/" >> ~/.zshrc
 test -d /usr/local/bin/ && echo "/usr/local/bin/" >> ~/.bashrc
@@ -12,11 +12,11 @@ echo "export PATH='/usr/local/bin/:$PATH'" >> ~/.zshrc
 echo "export PATH='/usr/local/bin/:$PATH'" >> ~/.bashrc
 
 # Stellar CLI Auto-Complete
-chmod 644 ~/.bashrc
 echo "source <(stellar completion --shell bash)" >>~/.bashrc
-chmod 644 ~/.zshrc
 echo "source <(stellar completion --shell zsh)" >>~/.zshrc
 echo "Enabled Stellar CLI auto-completion"
 
 echo " ✅ postStartCliAutocomplete.sh executed successfully"
+
+make
 
